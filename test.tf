caaaa09e63c8450ac92ed74913daa27f06c67dd2
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-09-04 09:47:04 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name" {
     ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.nano"
-    availability_zone = "us-east-1b"
+    availability_zone = "us-east-1c"
     tags = {
         Name = "test"
     }
